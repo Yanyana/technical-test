c6a7b08984bfa3c10f206ec287ad0d0ad53cbc37
yanyana <yanyana@github.com>
2022-11-29 23:23:57 +0700

    message broker use redisMQ

--- routes/email.route.js
+++ routes/email.route.js
@@ -1,29 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const redis = require("redis");
+const url = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+const client = redis.createClient({url});
+
+client.on("error", (err) => {
+    console.log("REDIS ERROR => "+ err);
+});
+
+client.connect();
+
 const { success, dataNotFound } = require("../utils/response-handler");
 const errorHandler = require("../utils/error-handler")
 const emailController = require("../controllers/emailController");
 
-router.get('/', async (req, res) => {
-    return success(res, {}, 'email here.')
-});
+redisQueue();
+
+async function redisQueue(){
+  const subscriber = client.duplicate();
+  await subscriber.connect();
+
+  await subscriber.subscribe('send_mail', async (message) => {
+    let dataMail = JSON.parse(JSON.parse(JSON.stringify(message)));
+    console.log('reading data from message queue')
+    await emailController.sendMail(dataMail)
+  });
+}
 
 router.post('/', async (req, res) => {
     try {
-        const {
-            email,
-            subject,
-            text,
-        } = req.body;
-
-        const mail = await emailController.sendMail({
-            email,
-            subject,
-            text
-        });
-    
-        if (mail) {
-            return success(res, mail, 'Item created successfully!')
+        let mail = await client.publish('send_mail', JSON.stringify(req.body));
+        // 0 is not yet read redis subscribe
+        if (mail >= 0) {
+            return success(res, req.body, 'Message Queue')
         } else {
             return dataNotFound(res, mail)
         }
